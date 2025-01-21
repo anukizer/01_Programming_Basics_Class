a48923310f4a3df383c0e5d316a240c50bfbc800
anukizer <anukakizer@gmail.com>
2025-01-20 22:13:12 -0500

    script.js

---
+++ 01_Programming_Basics/Day02/script.js
@@ -0,0 +1,21 @@
+/! Variable - storage/
+
+//? String - Hello World
+
+// Declaration and Initialization
+
+var  str = 'Hello World';
+console.log(str);
+
+//? Number
+var myNum = 123;
+console.log(myNum)
+
+//? Boolean - true or false
+
+var isStudent = true
+console.log(isStudent)
+//? Null
+
+var myName;
+console.log(myName)
